c3063fa52046de06eec409596debd2d491e6b99d
Alexandros Ntavelos <a.ntavelos@gmail.com>
2013-12-06 19:11:55 +0100

    adding new functions in arrayFactory

--- src/arrayFactory.js
+++ src/arrayFactory.js
@@ -2,12 +2,14 @@
 function ArrayFactoryException(message){
 	this.message = message;
 	this.toString = function(){
-		return message;
+		return 'ArrayFactoryException: ' + message;
 	};
 }
 
 /**
  * Returns the array with its unique elements.
+ * 
+ * @return {Array}
  */
 if (typeof Array.prototype.unique !== 'function') {
 	Array.prototype.unique = function() {
@@ -23,10 +25,11 @@ if (typeof Array.prototype.unique !== 'function') {
 }
 
 /**
- * Returns a dict with pairs of the unique values along with their 
- * frequencies.
+ * Returns a dict with pairs of the unique values along with their frequencies.
+ * 
+ * @return {Array}
  */
-if (typeof Array.prototype.frequencies !== 'function') {
+if (typeof Array.prototype.freq !== 'function') {
 	Array.prototype.frequencies = function() {
 		var frequencyArray = {};
 		
@@ -44,6 +47,8 @@ if (typeof Array.prototype.frequencies !== 'function') {
 
 /**
  * Returns the array shuffled.
+ *
+ * @return {Array}
  */
 if (typeof Array.prototype.shuffle !== 'function') {
 	Array.prototype.shuffle = function() {
@@ -59,6 +64,11 @@ if (typeof Array.prototype.shuffle !== 'function') {
 	}
 }
 
+/**
+ * Zips an array with another given as argument. The new array starts with the first element of the original array
+ * @param {Array} array - The with which the original array will be zipped
+ * @return {Array}  
+ */
 if (typeof Array.prototype.zip !== 'function') {
 	Array.prototype.zip = function(array) {
 		if (!(array instanceof Array)) {
@@ -91,6 +101,116 @@ if (typeof Array.prototype.zip !== 'function') {
 	}
 }
 
+
+/**
+ * Sets all the elements of the array equal to [value] 
+ * 
+ * @param {} value - The value that will replace all elements of the array.
+ * @return {Array} 
+ *  
+ */
+if (typeof Array.prototype.set !== 'function') {
+	Array.prototype.set = function(start, end, value) {
+		if (arguments.length != 3) {
+			throw new ArrayFactoryException('Function set needs 3 argument [start], [end], [value]');				
+		}
+
+		if (typeof start !== 'number' || typeof end !== 'number') {
+			throw new ArrayFactoryException('Arguments must be numbers.');		
+		}
+
+		if (typeof start < 0 || typeof end > this.length - 1 || typeof end < 0 || typeof start > this.length - 1) {
+			throw new ArrayFactoryException('Thresholds must be betweet array limits: 0 - array.length');		
+		}
+
+		if (start > end) {
+			throw new ArrayFactoryException('start must be lower that end');	
+		}
+		for (var i = start; i <= end; i++) {
+			this[i] = value;
+		}
+
+		return this;
+	}
+}
+
+
+/**
+ * Sets all the elements of the array equal to [value] 
+ * 
+ * @param {} value - The value that will replace all elements of the array.
+ * @return {Array} 
+ */
+if (typeof Array.prototype.setAll !== 'function') {
+	Array.prototype.setAll = function(value) {
+		if (value == null) {
+			throw new ArrayFactoryException('Function setAll needs an argument [value]');				
+		}
+		for (var i = 0; i < this.length; i++) {
+			this[i] = value;
+		}
+
+		return this;
+	}
+}
+
+/**
+ * Rotates the elements of the array [offset] positions left-wise or right-wise depending on the sign of offset.
+ * 
+ * @param {Number} offset - The offset of the rotate. 
+ *                          Negative offset means rotate left-wise and positive offset means shif right-wise. 
+ *                          Floats will be converted into integers.
+ * @return {Array} 
+ *                           
+ */
+if (typeof Array.prototype.rotate !== 'function') {
+	Array.prototype.rotate = function(offset) {
+		if (arguments.length != 1) {
+			throw new ArrayFactoryException('Function rotate: needs an argument [offset]');			
+		}
+		if (typeof offset !== 'number') {
+			throw new ArrayFactoryException('Function rotate: [offset] should be a number');				
+		}
+
+		// help function for moving 
+		var float = function(array, positions) {
+			for(var i = 0; i < positions; i++) {
+				var first = array.shift();
+				array.push(first);
+			}
+
+			return array;
+		}
+
+		if(Math.abs(offset) == this.length) {
+			return this;
+		}
+
+		offset = Math.floor(offset); // convert it into integer anyway
+		offset = offset % this.length // normalise offset larger that the array's length
+		var temp = this;
+
+		if (offset > 0) {
+			temp = float(temp.reverse(), offset);
+			temp = temp.reverse();
+		} else {
+			temp = float(temp, Math.abs(offset));
+		}
+		
+		return temp;
+	}
+}
+
+
+
+//------------------------------------Auxiliary functions----------------------------------------
+
+/**
+ * Returns a random number between two given numbers 
+ * @param  {Number} min The lower limit
+ * @param  {Number} max The upper limit
+ * @return {Number}
+ */
 function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
--- test/arrayFactory.test.js
+++ test/arrayFactory.test.js
@@ -2,20 +2,60 @@ var assert = require('assert'),
 	factory = require('../src/arrayFactory');
 
 module.exports = {
-	'unique': function() {
+	// Testing Array.prototype.unique()
+	'testUnique': function() {
 		assert.deepEqual([1,2,3,4], [1,2,2,3,3,2,1,1,2,3,4,3,3,3,4,3,3,2,2,1].unique());
 	},
-	'frequencies': function() {
+
+	// Testing Array.prototype.freq()
+	'testFrequencies': function() {
 		assert.deepEqual({1:1, 2:2, 3:3, 4:4}, [1,2,3,4,2,3,4,3,4,4].frequencies());
 	},
-	'shuffle': function() {
+
+	// Testing Array.prototype.shuffle()
+	'testShuffle': function() {
 		assert.deepEqual(false, [1,2,3,4,5].shuffle() === [1,2,3,4,5]);
 	},
-	'zip': function() {
-		// assert.deepEqual(-1, [1,2,3,4,5].zip('a'));
+
+	// Testing Array.prototype.zip()
+	'testZipEmptyArument': function() {
 		assert.deepEqual([1,2,3,4,5], [1,2,3,4,5].zip([]));
+	},
+	'testZipEmptyArray': function() {
 		assert.deepEqual([1,2,3,4,5], [].zip([1,2,3,4,5]));
+	},
+	'testZipNormal': function() {
 		assert.deepEqual([1,2,3,4,5,6,7,8,9], [1,3,5,7,9].zip([2,4,6,8]));
+	},
+	'testZipArraySmallerThanArument': function() {
 		assert.deepEqual([2,1,4,3,6,5,8,7,9], [2,4,6,8].zip([1,3,5,7,9]));
+	},
+
+	// Testing Array.prototype.set()
+	'testSetNormal': function() {
+		assert.deepEqual([1,2,'a','a','a',6,7,8], [1,2,3,4,5,6,7,8].set(2,4,'a'));
+	},
+
+	// Testing Array.prototype.setAll()
+	'testSetAll': function() {
+		assert.deepEqual(['a', 'a', 'a', 'a', 'a'], [1,2,3,4,5].setAll('a'));
+	},
+
+	// Testing Array.prototype.rotate()
+	'testRotateRight': function() {
+		assert.deepEqual([4,5,6,1,2,3], [1,2,3,4,5,6].rotate(3));
+	},
+	'testRotateLeft': function() {
+		assert.deepEqual([3,4,5,6,1,2], [1,2,3,4,5,6].rotate(-2));
+	},
+	'testRotateRightOffsetLongerThanLenth': function() {
+		assert.deepEqual([4,5,6,1,2,3], [1,2,3,4,5,6].rotate(9));
+	},
+	'testRotateLeftOffsetLongerThanLenth': function() {
+		assert.deepEqual([3,4,5,6,1,2], [1,2,3,4,5,6].rotate(-8));
+	},
+	'testRotateOffsetEqualToLength': function() {
+		assert.deepEqual([1,2,3,4,5,6], [1,2,3,4,5,6].rotate(6));
 	}
+
 }; 
